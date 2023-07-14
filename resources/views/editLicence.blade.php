bda777de0f23e4d8cd85f33668f253a1110103db
Onrcnds <onur_candas@hotmail.com>
2023-07-14 10:36:04 +0300

    tasarım

--- resources/views/editLicence.blade.php
+++ resources/views/editLicence.blade.php
@@ -1,33 +1,60 @@
-<form action="{{ route('licence.update', $licence->id) }}" method="POST">
-  @csrf
-  @method('PUT')
-  <div>
-    <label for="licence_name">Lisans Adı:</label>
-    <input type="text" id="licence_name" name="licence_name" value="{{$licence->lisansadi}}">
-  </div>
-  <!-- <div>
-    <label for="name">İsim:</label>
-    <input type="text" id="name" name="name" value="{{$licence->isim}}">
-  </div>
-  <div>
-    <label for="surname">Soyisim:</label>
-    <input type="text" id="surname" name="surname" value="{{$licence->soyisim}}">
-  </div>
-  <div>
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" value="{{$licence->email}}">
-  </div> -->
-  <div>
-    <label for="purchase_date">Alış Tarihi:</label>
-    <input type="date" id="purchase_date" name="purchase_date" value="{{$licence->aliştarihi}}">
-  </div>
-  <div>
-    <label for="duration">Süre:</label>
-    <input type="text" id="duration" name="duration" value="{{$licence->süre}}">
-  </div>
-  <div>
-    <label for="end_date">Bitiş Tarihi:</label>
-    <input type="date" id="end_date" name="end_date" value="{{$licence->bitiştarihi}}">
-  </div>
-  <button type="submit">Güncelle</button>
-</form>
+<style>
+  .form-container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f5f5f5;
+    border-radius: 5px;
+  }
+
+  .form-container label {
+    font-weight: bold;
+  }
+
+  .form-container input[type="text"],
+  .form-container input[type="date"] {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
+  .form-container button[type="submit"] {
+    background-color: #2c3e50;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .form-container button[type="submit"]:hover {
+    background-color: #2c3e50;
+  }
+</style>
+
+<div class="form-container">
+  <form action="{{ route('licence.update', $licence->id) }}" method="POST">
+    @csrf
+    @method('PUT')
+    <div>
+      <label for="licence_name">Lisans Adı:</label>
+      <input type="text" id="licence_name" name="licence_name" value="{{$licence->lisansadi}}">
+    </div>
+    <div>
+      <label for="purchase_date">Alış Tarihi:</label>
+      <input type="date" id="purchase_date" name="purchase_date" value="{{$licence->aliştarihi}}">
+    </div>
+    <div>
+      <label for="duration">Süre:</label>
+      <input type="text" id="duration" name="duration" value="{{$licence->süre}}">
+    </div>
+    <div>
+      <label for="end_date">Bitiş Tarihi:</label>
+      <input type="date" id="end_date" name="end_date" value="{{$licence->bitiştarihi}}">
+    </div>
+    <button type="submit">Güncelle</button>
+  </form>
+</div>
--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="tr">
   <head>
     <!-- Required meta tags -->
@@ -7,129 +7,135 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <title>Uygulama Lisansları</title>
-
+    <link rel="stylesheet" href="styles.css">
     <link href="index.css" rel="stylesheet">
   </head>
   <body>
 
   <nav class="navbar navbar-dark bg-mynav">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Lisans</a>
-    @auth
-      <div class="d-flex align-items-center">
-        <span class="navbar-text me-3">
-          Hoş geldin, {{ Auth::user()->name }}
-        </span>
-        <form action="{{ route('logout') }}" method="POST">
-          @csrf
-          <button type="submit" class="btn btn-link text-white">Çıkış Yap</button>
-        </form>
-      </div>
-    @endauth
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Lisans</a>
+      @auth
+        <div class="d-flex align-items-center">
+          <span class="navbar-text me-3">
+            Hoş geldin, {{ Auth::user()->name }}
+          </span>
+          <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-link text-white">Çıkış Yap</button>
+          </form>
+        </div>
+      @endauth
+    </div>
+  </nav>
+
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Lisans Adı</th>
+          <th scope="col">İsim</th>
+          <th scope="col">Email</th>
+          <th scope="col">Başlangıç</th>
+          <th scope="col">Bitiş</th>
+          <th scope="col">Süre</th>
+          <th scope="col">Kalan Süre</th>
+          <th scope="col">Durum</th>
+          <th scope="col">Düzenle/Sil</th>
+        </tr>
+      </thead>
+      <tbody id="mytable">
+        @foreach($licences as $licence)
+          <?php
+            $sıra = $loop->index + 1;
+            $kalansüre = strtotime($licence->bitiştarihi) - strtotime('now');
+            $toplamSure =$licence->süre;
+            if ($toplamSure <30)
+            {
+              $toplamSure = $toplamSure." Ay";
+            }
+            else if ($toplamSure <365)
+            {
+              $toplamSure = floor($toplamSure / 30)." Ay";
+            }
+          ?>
+          <tr>
+            <th scope="row">{{$sıra}}</th>
+            <td>{{$licence->lisansadi}}</td>
+            <td>{{$licence->isim}}</td>
+            <td>{{$licence->email}}</td>
+            <td>{{$licence->aliştarihi}}</td>
+            <td>{{$licence->bitiştarihi}}</td>
+            <td>{{$toplamSure}}</td>
+            <td>
+              @if($kalansüre < 0)
+                <span style="color: red;">Süre Doldu</span>
+              @else
+                <span style="color: green;">{{floor($kalansüre / 86400)}} Gün</span>
+              @endif
+            </td>
+            <td>
+              <div style="display: flex; align-items: center;">
+                @if(strtotime($licence->bitiştarihi) < strtotime('now'))
+                  <div style="background-color: red; width: 20px; height: 20px; border-radius: 50%;"></div>
+                  <span style="margin-left: 5px;">Pasif</span>
+                @else
+                  <div style="background-color: green; width: 20px; height: 20px; border-radius: 50%;"></div>
+                  <span style="margin-left: 5px;">Aktif</span>
+                @endif
+              </div>
+            </td>
+            <td>
+            <a href="{{ route('licence.edit', $licence->id) }}" class="btn btn-primary">
+  <i class="fas fa-pencil-alt"></i>
+</a>
+
+              <form action="{{ route('licence.delete', $licence->id) }}" method="POST" style="display: inline;">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+  <i class="fas fa-trash"></i>
+</button>
+
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
   </div>
-</nav>
 
-
-
-
-    <div class="container">
-      <div class="d-flex bd-highlight mb-3">
-        <div class="me-auto p-2 bd-highlight"><h2>Kullanıcı</div>
-        <div class="p-2 bd-highlight">
-          <button type="button" class="btn btn-secondary" onclick="showUserCreateBox()">Ekle</button>
+  <div class="container">
+    <h2>Kayıt Formu</h2>
+    <form id="registrationForm" method="POST" action="/createLicence">
+      @csrf
+      <div class="form-row">
+        <div class="form-group">
+          <label for="licence_name">Lisans Adı</label>
+          <input id="licence_name" class="form-control" type="text" name="licence_name" placeholder="Lisans Adı" required>
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input id="email" class="form-control" type="email" name="email" placeholder="Email" required>
+        </div>
+        <div class="form-group">
+          <label for="purchase_date">Alış Tarihi</label>
+          <input id="purchase_date" class="form-control" type="date" name="purchase_date" required>
+        </div>
+        <div class="form-group">
+          <label for="end_date">Bitiş Tarihi</label>
+          <input id="end_date" class="form-control" type="date" name="end_date" required>
         </div>
       </div>
-
-      <div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Lisans Adı</th>
-        <th scope="col">İsim</th>
-        <th scope="col">Email</th>
-        <th scope="col">Başlangıç</th>
-        <th scope="col">Bitiş</th>
-        <th scope="col">Süre</th>
-        <th scope="col">Kalan Süre</th>
-        
-        <th scope="col">Durum</th>
-      </tr>
-    </thead>
-    <tbody id="mytable">
-      
-      @foreach($licences as $licence)
-        <?php
-          $sıra = $loop->index + 1;
-          $kalansüre = strtotime($licence->bitiştarihi) - strtotime('now');
-          $toplamSure =$licence->süre;
-          if ($toplamSure <30)
-          {
-            $toplamSure = $toplamSure." Ay";
-          }
-          else if ($toplamSure <365)
-          {
-            $toplamSure = floor($toplamSure / 30)." Ay";
-          }
-        ?>
-      <tr>
-  <th scope="row">{{$sıra}}</th>
-  <td>{{$licence->lisansadi}}</td>
-  <td>{{$licence->isim}}</td>
-  <td>{{$licence->email}}</td>
-  <td>{{$licence->aliştarihi}}</td>
-  <td>{{$licence->bitiştarihi}}</td>
-  <td>{{$toplamSure}} </td>
-  <td>
-    @if($kalansüre < 0)
-      <span style="color: red;">Süre Doldu</span>
-    @else
-      <span style="color: green;">{{floor($kalansüre / 86400)}} Gün</span>
-    @endif
-  <td>
-    <div style="display: flex; align-items: center;">
-      @if(strtotime($licence->bitiştarihi) < strtotime('now'))
-        <div style="background-color: red; width: 20px; height: 20px; border-radius: 50%;"></div>
-        <span style="margin-left: 5px;">Pasif</span>
-      @else
-        <div style="background-color: green; width: 20px; height: 20px; border-radius: 50%;"></div>
-        <span style="margin-left: 5px;">Aktif</span>
-      @endif
-    </div>
-  </td>
-  <td>
-    <a href="{{ route('licence.edit', $licence->id) }}" class="btn btn-primary">Düzenle</a>
-    <form action="{{ route('licence.delete', $licence->id) }}" method="POST" style="display: inline;">
-      @csrf
-      @method('DELETE')
-      <button type="submit" class="btn btn-danger">Sil</button>
+      <button type="submit" class="btn2">Kaydet</button>
     </form>
-  </td>
-</tr>
+  </div>
 
-      @endforeach
-    </tbody>
-  </table>
-</div>
-
-    
-    <form id="registrationForm" method="POST"  action="/createLicence"> 
-      @csrf 
-      <input id="id" type="hidden" name="id"> 
-      <input id="licence_name" class="swal2-input" placeholder="Lisans Adı" name="licence_name"> 
-      <!-- <input id="name" class="swal2-input" placeholder="İsim" name="name"> 
-      <input id="surname" class="swal2-input" placeholder="Soyisim" name="surname">  -->
-      <input id="email" class="swal2-input" placeholder="Email" name="email">  
-      <input id="purchase_date" class="swal2-input" placeholder="Alış Tarihi" name="purchase_date">
-      <input id="end_date" class="swal2-input" placeholder="Bitiş Tarihi" name="end_date">
-      
-      <button type="submit">Kaydet</button> 
-      </form>
-
-    <script src="index.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.16/dist/sweetalert2.all.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  <script src="index.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.16/dist/sweetalert2.all.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   </body>
 </html>
